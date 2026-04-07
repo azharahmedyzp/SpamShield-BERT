677e7ee58789d182b36de5fe32474b844cecce44
Azhar Ahmed Pathan <azharahmedyzp@gmail.com>
2026-04-08 01:30:24 +0500

    Add files via upload

---
+++ spam_detector.py
@@ -0,0 +1,20 @@
+from transformers import pipeline
+import torch
+
+device = 0 if torch.cuda.is_available() else -1
+print(f"Device: {device}")
+
+spam_pipeline = pipeline(
+    "text-classification",
+    model="mrm8488/bert-tiny-finetuned-sms-spam-detection",
+    device=device
+)
+
+def detect_spam(email_text):
+  result = spam_pipeline(email_text)[0]
+  label = "SPAM" if result["label"] == "LABEL_1" else "non-SPAM"
+  confidence = round(result["score"] * 100, 2)
+  return {
+      "label": label,
+      "confidence": confidence
+  }
